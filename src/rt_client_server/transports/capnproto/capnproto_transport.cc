2b79a193037acf2176f6c30c7e7b8e7ee8bd3f9d
Miller, Matt (SCON) <Matt.Miller2@netapp.com>
2020-05-11 18:32:58 -0400

    Start of capnproto transport

--- src/rt_client_server/transports/capnproto/capnproto_transport.cc
+++ src/rt_client_server/transports/capnproto/capnproto_transport.cc
@@ -1,18 +1,91 @@
 /* First, so it stays self-compiling */
 #include "capnproto_transport.hpp"
 
+#include "capnproto_transport.capnp.h"
+
+#include <kj/debug.h>
+#include <capnp/ez-rpc.h>
+#include <capnp/message.h>
+
 namespace rt {
 
 struct CapnprotoServer::impl final {
     explicit impl(std::string address, uint16_t port);
 
+    std::unique_ptr<capnp::EzRpcServer> _server;
+
 private:
 
+    struct ReqSvcImpl final : public StreamService::RequestCallback::Server {
+
+        kj::Promise<void> sendChunk(SendChunkContext context) override;
+        kj::Promise<void> done(DoneContext context) override;
+
+    private:
+
+        static kj::Promise<void> sendData(
+            StreamService::ReplyCallback::Client stream,
+            const std::vector<std::string> &vec, uint32_t strNum);
+
+        std::vector<std::string> _vec;
+    };
+
+    struct StreamServiceImpl final : public StreamService::Server {
+        kj::Promise<void> reqReply(ReqReplyContext context) override;
+    };
 };
+
+kj::Promise<void>
+CapnprotoServer::impl::StreamServiceImpl::reqReply(ReqReplyContext context)
+{
+    context.getResults().setReqSvc(kj::heap<ReqSvcImpl>());
+    return kj::READY_NOW;
+}
+
+kj::Promise<void>
+CapnprotoServer::impl::ReqSvcImpl::sendChunk(SendChunkContext context)
+{
+    auto chunk = context.getParams().getChunk();
+    // This makes a deep copy of the string's data
+    _vec.push_back(std::string(reinterpret_cast<const char *>(chunk.begin()),
+                               chunk.size()));
+    return kj::READY_NOW;
+}
+
+kj::Promise<void>
+CapnprotoServer::impl::ReqSvcImpl::done(DoneContext context)
+{
+    auto replySvc = context.getParams().getReplySvc();
+    return sendData(replySvc, _vec, 0);
+}
+
+kj::Promise<void>
+CapnprotoServer::impl::ReqSvcImpl::sendData(
+    StreamService::ReplyCallback::Client stream,
+    const std::vector<std::string> &vec, uint32_t strNum)
+{
+    if (strNum == vec.size()) {
+        return stream.doneRequest().send().ignoreResult();
+    }
+
+    auto chunkReq = stream.sendChunkRequest();
+    // TODO
+    //chunkReq.setChunk(capnp::Data::Reader(vec[strNum].c_str()));
+    return chunkReq.send().then([stream=kj::mv(stream),
+                                strNum, &vec]() mutable {
+            return sendData(kj::mv(stream), vec, strNum + 1);
+        });
+}
 
 CapnprotoServer::impl::impl(std::string address, uint16_t port)
 {
-    // TODO: start listener
+    const auto serverAddress = address + ":" + std::to_string(port);
+    _server =
+        std::make_unique<capnp::EzRpcServer>(kj::heap<StreamServiceImpl>(),
+                                             serverAddress);
+    if (nullptr == _server) {
+        throw std::runtime_error("server was not created");
+    }
 }
 
 CapnprotoServer::CapnprotoServer(std::string address, uint16_t port) :
@@ -24,7 +97,8 @@ CapnprotoServer::~CapnprotoServer() = default;
 void
 CapnprotoServer::wait()
 {
-    // TODO: block for requests
+    auto &waitScope = _pImpl->_server->getWaitScope();
+    kj::NEVER_DONE.wait(waitScope);
 }
 
 struct CapnprotoClient::impl final {
